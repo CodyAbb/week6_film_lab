48fc11ec3a4ce19f897c769d3cdc42174dcdb9e3
Cody Abbott <cody.abbott16@gmail.com>
2020-01-08 14:28:15 +0000

    completed search questions

--- models/cinema.js
+++ models/cinema.js
@@ -7,4 +7,18 @@ Cinema.prototype.listTitles = function () {
   return titleArray;
 };
 
+Cinema.prototype.findByTitle = function (searchTitle) {
+  const searchedObject = this.films.filter((film) => {
+    return film.title === searchTitle;
+  });
+  return searchedObject[0];
+};
+
+Cinema.prototype.findByGenre = function (searchGenre) {
+  const searchedObjects = this.films.filter((film) => {
+    return film.genre === searchGenre;
+  });
+  return searchedObjects;
+};
+
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -33,10 +33,16 @@ describe('Cinema', function () {
     assert.deepEqual(actual, cinema.listTitles());
   });
 
+  it('should be able to find a film by title', function(){
+    const actual = moonlight;
+    assert.deepEqual(actual, cinema.findByTitle('Moonlight'));
+  });
 
+  it('should be able to filter films by genre', function(){
+    const actual = [moonlight, trainspotting];
+    assert.deepStrictEqual(actual, cinema.findByGenre('drama'));
+  });
 
-  it('should be able to find a film by title');
-  it('should be able to filter films by genre');
   it('should be able to check whether there are some films from a particular year');
   it('should be able to check whether there are no films from a particular year');
   it('should be able to check whether all films are over a particular length');
