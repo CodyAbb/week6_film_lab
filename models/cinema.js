0fbd7b92b6bd285fdd6a8c2085fe663e4dc1fd33
Cody Abbott <cody.abbott16@gmail.com>
2020-01-08 14:15:23 +0000

    got first test passing

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,9 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.listTitles = function () {
+  const titleArray = this.films.map(film => film.title)
+  return titleArray;
+};
+
 module.exports = Cinema;
